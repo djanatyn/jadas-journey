853204a90763258e9c43c4e64c77ea6521534714
Jonathan Strickland <djanatyn@gmail.com>
2021-05-01 18:35:37 -0400

    test suite runs against all tweets

--- tests/test.hs
+++ tests/test.hs
@@ -1,16 +1,28 @@
 -- |
 module Test where
 
+import Data.Maybe
 import Jada.Journey hiding (main)
+import Lens.Micro
 import Test.Tasty
 import Test.Tasty.HUnit
+import Text.Megaparsec
+import Web.Tweet
 import Web.Tweet.Utils (displayTimelineColor)
 
-main =
+testParse :: TweetEntity -> TestTree
+testParse entity =
+  let tweet :: String
+      tweet = entity ^. text
+
+      parsed :: Maybe JadaTweet
+      parsed = parseMaybe pTweet tweet
+   in testCase "parsing tweets" $
+        assertBool
+          ("could not parse tweet: " ++ tweet)
+          (isJust parsed)
+
+main = do
+  tweets <- loadTweets "tweet.store"
   defaultMain $
-    testGroup
-      "tests"
-      [ testCase "load tweets" $ do
-          tweets <- loadTweets "tweet.store"
-          putStrLn . displayTimelineColor $ tweets
-      ]
+    testGroup "tests" (testParse <$> tweets)
