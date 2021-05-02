b0cbd49621168ead9e0f55a5d1c95b8041c48ee4
Jonathan Strickland <djanatyn@gmail.com>
2021-05-01 22:25:49 -0400

    qualified imports for tests

--- tests/test.hs
+++ tests/test.hs
@@ -1,15 +1,13 @@
 -- |
 module Test where
 
-import Data.Either
 import Jada.Journey hiding (main)
-import Lens.Micro
-import Test.Tasty
-import Test.Tasty.HUnit
-import Test.Tasty.Runners.Html
-import Text.Megaparsec
-import Web.Tweet
-import Web.Tweet.Utils (displayTimelineColor)
+import Lens.Micro ((^.))
+import Test.Tasty (TestTree, defaultIngredients, defaultMainWithIngredients, testGroup)
+import Test.Tasty.HUnit (assertFailure, testCase)
+import Test.Tasty.Runners.Html (htmlRunner)
+import Text.Megaparsec (errorBundlePretty, parse)
+import Web.Tweet (TweetEntity, text)
 
 testParse :: TweetEntity -> TestTree
 testParse entity =
